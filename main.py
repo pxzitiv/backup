9e72fa55cc733484bdf6a84f8cb3b2c08631cf63
pxzitiv <80686013+pxzitiv@users.noreply.github.com>
2021-05-11 17:01:54 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
+from flask import Flask, render_template, request
+app = Flask(__name__)
 
+@app.route('/', methods=['POST', 'GET'])
+def home():
+    if request.method == 'POST':
+        name = request.form['un']
+        pw = request.form['pw']
+        return 'hello ' + name + ' your password is ' + pw
+    else:
+        return render_template('simple_form.html')
